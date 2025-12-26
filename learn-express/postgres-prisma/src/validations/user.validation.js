9a7833db9b1cf9e1fad2ec3fe14ea80028c06202
himaana <himana.on@gmail.com>
2025-12-26 18:08:17 +0700

    feat: improve request validation error message

--- learn-express/postgres-prisma/src/validations/user.validation.js
+++ learn-express/postgres-prisma/src/validations/user.validation.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
     body: z.object({
-        name: z.string().min(1, "Name is required"),
+        name: z.string("Name must be a string").min(1, "Name is required"),
         email: z.email("Invalid email format"),
     }),
 });
